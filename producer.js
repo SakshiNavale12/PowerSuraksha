0d375b23e3c8c097f03b315f37fd1fc09d9b042b
UmeshpJadhav <umesh2005jadhav@gmail.com>
2025-09-26 23:51:13 +0530

    removeed await code

--- producer.js
+++ producer.js
@@ -24,10 +24,6 @@ console.info("Producer connected to Kafka");
         messages.push({ value: JSON.stringify(data) });
   }
 
-await producer.send({
-   topic: config.kafka.topic,
-  messages,
-});
 
       console.info(`Sent batch of 100 motor readings at ${new Date().toLocaleTimeString()}`);
     }, 2000);
